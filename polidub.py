951813d92d6dda760aa071410b7dfb3a4b2a4c12
Michael <wanderer.x64@gmail.com>
2019-11-05 20:04:08 +0200

    coding style changes

--- polidub.py
+++ polidub.py
@@ -1,102 +1,110 @@
-import os, hashlib, sqlite3
-
-# User entity
-class Member:
-
-    # User's storage entity
-    class Storage:
-
-        # initialize user's storage
-        def __init__(self, ip):
-            # Encoding user's IP to be hashed and stored as ID
-            encoded_ip = ip.encode('utf-8')
-            ID = hashlib.md5(encoded_ip)
-            # Declaring storage field
-            self.ID = ID
-            self.__connection = None
-            self.__cursor = None
-
-        # get user's storage ID
-        def get_ID(self):
-            return self.ID.hexdigest()
-
-        # write file to user's storage
-        def write_file(self, file):
-            # Setting path to DB
-            path_to_DB = os.path.join(os.getcwd(), self.get_ID(), "storage.db")
-
-            # Connecting to storage DB
-            self.__connection = sqlite3.connect(path_to_DB)
-            self.__cursor = self.__connection.cursor()
-            # Generate fileID
-            filename = file.name
-            fhash = hashlib.md5(filename.encode('utf-8'))
-            fID = fhash.hexdigest()
-            # Create table (file)
-            sql_command = 'CREATE TABLE ' + 'f' + fID + '(id INTEGER PRIMARY KEY, file BLOB NOT NULL, ' \
-                          + 'filename TEXT);'
-            self.__cursor.execute(sql_command)
-
-            # Set values of table's items
-            sql_command = 'INSERT INTO ' + 'f' + fID + '(file, filename)' \
-                         + 'VALUES (?, ?);'
-            cluster = ( file.read(), self.get_ID() )
-            # Commit changes
-            self.__cursor.execute(sql_command, cluster)
-            self.__connection.commit()
-
-        # get file from user's storage
-        def get_file_data(self, fID):
-            self.__cursor.execute("SELECT * FROM " + fID)
-            return self.__cursor.fetchall()
-
-        # get all files from user's storage
-        def list_files(self):
-            self.__cursor.execute('SELECT name from sqlite_master where type= "table"')
-            return self.__cursor.fetchall()
+import sqlite3
+import hashlib
+import os
+import random
 
 
-    # initialize member
+# DataBase entity
+class DB:
+    def __init__(self, name):
+        self.__name = name
+        self.connection = None    #TO DO
+        self.cursor = None
+
+    def connect_to_db(self, path_to_db):
+        self.connection = sqlite3.connect( os.path.join(path_to_db, self.__name) )
+        self.cursor = self.connection.cursor()
+
+
+
+# Member entity
+class Member(DB):
     def __init__(self, password, ip, plan):
-        # Initialize entity's fields
-        self.__password = password
-        self.__ip = ip
-        self._plan = plan
-        self.storage = self.Storage(ip)
-        self.__connection = None            #TO DO
-        self.__cursor = None
+        super(Member, self).__init__('member_info.db')
 
+        self.password = password
+        self.member_id = ip.replace('.', '')
+        self.plan = plan
 
-    # write member's data
     def write_member_data(self):
-        ID = self.storage.get_ID()
-        self.__connection = sqlite3.connect(os.path.join(os.getcwd(), ID, "user_info.db"))  # TO DO
-        self.__cursor = self.__connection.cursor()
+        sql_command = 'CREATE TABLE ' + 'm' + self.member_id \
+                      + '(userid INTEGER PRIMARY KEY,' \
+                      + 'password VARCHAR(30),' \
+                      + 'ip VARCHAR(15), plan CHAR(1));'
 
-        sql_command = 'CREATE TABLE ' + 'm' + self.storage.get_ID() + '(userid INTEGER PRIMARY KEY,' \
-                                                                    + 'password VARCHAR(30),' \
-                                                                    + 'ip VARCHAR(15), plan CHAR(1));'# TO DO
-        self.__cursor.execute(sql_command)
-        member = [self.__password, self.__ip, self._plan]
-        format_str = 'INSERT INTO ' + 'm' + self.storage.get_ID() + '(userid, password, ip, plan)'\
-                + 'VALUES (NULL, "{}", "{}", "{}");'
-        sql_command = format_str.format(member[0], member[1], member[2]) #TO DO
-        self.__cursor.execute(sql_command)
-        self.__connection.commit()
+        self.cursor.execute(sql_command)
+        format_str = 'INSERT INTO ' + 'm' + self.member_id \
+                     + '(userid, password, ip, plan)' \
+                     + 'VALUES (NULL, "{}", "{}", "{}");'
+
+        sql_command = format_str.format(self.password, self.member_id, self.plan)
+        self.cursor.execute(sql_command)
+        self.connection.commit()
 
     def get_member_data(self):
-        self.__cursor.execute("SELECT * FROM member")
-        return self.__cursor.fetchall()
-
-    def write_member_file(self, file):
-        self.storage.write_file(file)
-
-    def list_member_files(self):
-        return self.storage.list_files()
-
-    def get_member_file(self, fID):
-        return self.storage.get_file_data(fID)
+        self.cursor.execute("SELECT * FROM member")
+        return self.cursor.fetchall()
 
 
 
+# Storage entity
+class Storage(DB):
+    def __init__(self, ID):
+        super(Storage, self).__init__('storage.db')
+        self.ID = ID
 
+    @staticmethod
+    def generate_file_id():
+        random_num = str(random.random() * 100000)
+        fhash = hashlib.md5(random_num.encode('utf-8'))
+        return fhash.hexdigest()
+
+    def file_exists(self, file_id):
+        if ('f' + file_id,) in self.list_files():
+            return True
+        else:
+            return False
+
+    def list_files(self):
+        self.cursor.execute('SELECT name from '
+                            + 'sqlite_master where '
+                            + 'type= "table"')
+        return self.cursor.fetchall()
+
+    def get_file_security(self, file_id):
+        if self.file_exists(file_id):
+            self.cursor.execute('SELECT slv from ' + 'f' + file_id)
+            return self.cursor.fetchall()[0][0]
+
+    def get_file_passwrod(self, file_id):
+        if self.file_exists(file_id):
+            self.cursor.execute('SELECT ps from ' + 'f' + file_id)
+            return self.cursor.fetchall()[0][0]
+
+    def write_file(self, file, passwrod=None):
+        if passwrod:
+            if not type(passwrod) == str:
+                raise ValueError()
+        file_id = Storage.generate_file_id()
+        if self.file_exists(file_id):
+            raise FileExistsError()
+
+        if passwrod:
+            security = 'p'
+        else:
+            security = 'd'
+
+        sql_command = 'CREATE TABLE ' + 'f' + file_id \
+                      + '(id INTEGER PRIMARY KEY, ' \
+                      + 'file BLOB NOT NULL, ' \
+                      + 'filename TEXT, ' \
+                      + 'slv CHAR(1), ps TEXT);'
+
+        self.cursor.execute(sql_command)
+        sql_command = 'INSERT INTO ' + 'f' + file_id \
+                      + '(file, filename, slv, ps)' \
+                      + 'VALUES (?, ?, ?, ?);'
+
+        cluster = (file.read(), file.name, security, passwrod)
+        self.cursor.execute(sql_command, cluster)
+        self.connection.commit()
